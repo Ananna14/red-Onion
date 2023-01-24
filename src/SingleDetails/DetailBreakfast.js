db27adc199a5b6cbd2f545de26d182fe4a5f8cf5
Ananna14 <https://github.com/Ananna14>
2023-01-24 20:53:51 +0600

    icon-design-added

--- src/SingleDetails/DetailBreakfast.js
+++ src/SingleDetails/DetailBreakfast.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { breakfastsData } from '../FilterData/Breakfast';
 // import breakfastDetails from './breakfast.json'
 import { FaCartPlus } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
+import { FaMinus } from 'react-icons/fa';
+import './DetailLunch.css'
 
 const DetailBreakfast = () => {
   const {id} = useParams();
@@ -35,11 +38,27 @@ useEffect(()=>{
       <div className="col-12 col-lg-6 p-5 mt-5">
         <p className="card-title fs-2 fw-bold text-start">{bodyData.name}</p>
         <p className="card-text text-start">{bodyData.details}</p>
-        <p className='fs-2 price-text text-start'>${bodyData.price}</p>
+        {/* icon-added-start */}
+        {/* <div className='d-flex'> */}
+          {/* <div> */}
+            
+          {/* </div> */}
+          <div className='d-flex'>
+            <div>
+              <h6 className='fs-2 price-text text-start'>${bodyData.price}</h6>
+            </div>
+              <div className='rounded-pill d-flex icon-added align-items-center'>
+              <h6 className=''><span className='px-4 py-1'><FaPlus/></span></h6>
+              <h6 className=''><span className="px-4 py-1"><FaMinus/></span></h6>
+            </div>
+          </div>
+        {/* </div> */}
+        {/* icon-added-end */}
+   
           {/* btn-added-start */}
-          <div className='text-start'>
-            <button className="mx-1 single-btn p-1 rounded"><span className="mx-1"><FaCartPlus/></span><span  className="mx-1">ADD</span></button>
-            <button className="mx-1 single-btn p-1 rounded blue-btn"><span  className="mx-1">ADD MORE</span></button>
+          <div className='text-start mt-4'>
+            <button className="mx-1 single-btn p-1 rounded-pill px-3"><span className="mx-1"><FaCartPlus/></span><span  className="mx-1">ADD</span></button>
+           <Link to="/"><button className="mx-1 single-btn p-1 rounded-pill px-3 blue-btn"><span  className="mx-1">ADD MORE</span></button></Link>
          </div>
           {/* btn-added-end */}
       </div>
