4eac2faebe5fec2e2303acc49f45264b29f6b1c4
Ananna14 <https://github.com/Ananna14>
2023-01-25 20:33:39 +0600

    incrementing-adde

--- src/SingleDetails/DetailBreakfast.js
+++ src/SingleDetails/DetailBreakfast.js
@@ -15,8 +15,9 @@ useEffect(()=>{
   const filtered =breakfastsData.filter(breakfast => breakfast.id == id)
   if(filtered.length){
     setBodyData(filtered[0]);
-  }
+  }                                
 },[])
+
 // console.log(bodyData)
 
 // 2nd-way
@@ -27,6 +28,20 @@ useEffect(()=>{
 // }
 // },[id])
 // console.log(bodyData)
+
+const [num, setNum] = useState(0);
+
+const incNum = () =>{
+  setNum(num + 1);
+}
+const decNum = () =>{
+  if(num>0){
+    setNum(num - 1);
+  }
+  else{
+    setNum(0)
+  }
+}
  
   return (
   <div className='mt-5'>
@@ -39,25 +54,21 @@ useEffect(()=>{
         <p className="card-title fs-2 fw-bold text-start">{bodyData.name}</p>
         <p className="card-text text-start">{bodyData.details}</p>
         {/* icon-added-start */}
-        {/* <div className='d-flex'> */}
-          {/* <div> */}
-            
-          {/* </div> */}
           <div className='d-flex'>
             <div>
               <h6 className='fs-2 price-text text-start'>${bodyData.price}</h6>
             </div>
               <div className='rounded-pill d-flex icon-added align-items-center'>
-              <h6 className=''><span className='px-4 py-1'><FaPlus/></span></h6>
-              <h6 className=''><span className="px-4 py-1"><FaMinus/></span></h6>
+              <h6 onClick={decNum}><span className="px-4 py-1"><FaMinus/></span></h6>
+              <h6>{num}</h6>
+              <h6 onClick={incNum}><span className='px-4 py-1'><FaPlus/></span></h6>
             </div>
           </div>
-        {/* </div> */}
         {/* icon-added-end */}
    
           {/* btn-added-start */}
           <div className='text-start mt-4'>
-            <button className="mx-1 single-btn p-1 rounded-pill px-3"><span className="mx-1"><FaCartPlus/></span><span  className="mx-1">ADD</span></button>
+            <button id="case-plus" className="mx-1 single-btn p-1 rounded-pill px-3"><span className="mx-1"><FaCartPlus/></span><span  className="mx-1">ADD</span></button>
            <Link to="/"><button className="mx-1 single-btn p-1 rounded-pill px-3 blue-btn"><span  className="mx-1">ADD MORE</span></button></Link>
          </div>
           {/* btn-added-end */}
--- src/SingleDetails/DetailDinner.js
+++ src/SingleDetails/DetailDinner.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { dinnerData } from '../FilterData/DinnerData';
 import { FaCartPlus } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
+import { FaMinus } from 'react-icons/fa';
 
 const DetailDinner = () => {
     const [bodyData, setBodyData] = useState(null);
@@ -15,6 +17,21 @@ useEffect(()=>{
     },[id])
       // console.log(bodyData)
 
+      // incrementing & decrementing
+    const [num, setNum] = useState(0);
+    const incNum =()=>{
+      setNum(num + 1);
+    }
+
+    const decNum=()=>{
+     if(num>0){
+      setNum(num-1)
+     }
+     else{
+      setNum(0)
+     }
+    }
+
   return (
    <div className='mt-5'>
      {
@@ -25,11 +42,22 @@ useEffect(()=>{
       <div className="col-12 col-lg-6 p-5 mt-5">
           <p className="card-title fs-2 fw-bold text-start">{bodyData.name}</p>
           <p className="card-text text-start">{bodyData.details}</p>
-          <p className='fs-2 price-text text-start'>${bodyData.price}</p>
+           {/* icon-added-start */}
+           <div className='d-flex'>
+            <div>
+              <h6 className='fs-2 price-text text-start'>${bodyData.price}</h6>
+            </div>
+              <div className='rounded-pill d-flex icon-added align-items-center'>
+              <h6 onClick={decNum}><span className="px-4 py-1"><FaMinus/></span></h6>
+              <h6>{num}</h6>
+              <h6 onClick={incNum}><span className='px-4 py-1'><FaPlus/></span></h6>
+            </div>
+          </div>
+        {/* icon-added-end */}
            {/* btn-added-start */}
-         <div className='text-start'>
+         <div className='text-start mt-4'>
             <button className="mx-1 single-btn p-1 rounded"><span className="mx-1"><FaCartPlus/></span><span  className="mx-1">ADD</span></button>
-            <button className="mx-1 single-btn p-1 rounded blue-btn"><span  className="mx-1">ADD MORE</span></button>
+            <Link to="/"><button className="mx-1 single-btn p-1 rounded-pill px-3 blue-btn"><span  className="mx-1">ADD MORE</span></button></Link>
          </div>
           {/* btn-added-end */}
         </div>
