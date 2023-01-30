bf186f201973c26593a40a228e1c05827cf0341b
Ananna14 <https://github.com/Ananna14>
2023-01-30 13:22:47 +0600

    design-alignment

--- src/CartItem/CartItem.js
+++ src/CartItem/CartItem.js
@@ -8,44 +8,39 @@ const CartItem = () => {
   return (
     <div className='row container d-flex justify-content-center'>
         {/* 1st-part */}
+     
+
         <div className='col-lg-6'>
             <div className="mt-5 mb-3">
-                <input type="text" placeholder='Deliver to door' className='w-100 h-5 input-btn p-2' name="" id="" />
+                <h6 className='mb-3 delivery-text'>Edit Delivery Details</h6>
+                <input type="text" placeholder='Deliver to door' className='w-75 h-5 input-btn p-2' name="" id="" />
             </div>
             <div className="mb-3">
-                <input type="text" placeholder='Road No. 12' className='w-100 h-5 input-btn p-2' name="" id="" />
+                <input type="text" placeholder='Road No. 12' className='w-75 h-5 input-btn p-2' name="" id="" />
             </div>
             <div className="mb-3">
-                <input type="text" placeholder='Flat site or floor' className='w-100 h-5 input-btn p-2' name="" id="" />
+                <input type="text" placeholder='Flat site or floor' className='w-75 h-5 input-btn p-2' name="" id="" />
             </div>
             <div className="mb-3">
-                <input type="text" placeholder='Business Name' className='w-100 h-5 input-btn p-2' name="" id="" />
+                <input type="text" placeholder='Business Name' className='w-75 h-5 input-btn p-2' name="" id="" />
             </div>
             <div className="mb-3">
-                <input type="type" placeholder='Add Delivery Instructor' className='w-100 input-btn p-2' name="" id="" />
+                <input type="type" placeholder='Add Delivery Instructor' className='w-75 input-btn p-2' name="" id="" />
             </div>
-                <button className="btn w-100 btn-form">SAVE AND CONTINUE</button>
+                <button className="btn w-75 btn-form">SAVE AND CONTINUE</button>
         </div>
         {/* 2nd-part */}
         <div className="col-lg-6 mt-5">
-                <p>FromGulsan Plaza Restaurant GPR<br/>
-                        Arriving in 20 - 30 min<br/>
-                        107 Rd No 8</p>
+             
                 <div>
                    {
-                        state.cart.map(item=><CartItemDelivery
+                        state.cart.map(item=><CartItemDelivery 
                         item={item}
                         state={state}
                         key={item.id}
                         ></CartItemDelivery>)
                    }
                 </div>
-            <div>
-           <div>
-
-
-           </div>
-            </div>
         </div>
     </div>
   )
--- src/CartItem/CartItemDelivery.js
+++ src/CartItem/CartItemDelivery.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useStore } from '../App';
+import './cartItemDelivery.css'
 
 const CartItemDelivery = (props) => {
     // console.log(props)
@@ -8,11 +8,31 @@ const CartItemDelivery = (props) => {
     
     // console.log(state);
   return (
-    <div>
-       <div>
-       <h1>{name}</h1>
-       </div>
-    </div>
+    <div className="res-plaza">
+        <p className='text-start'>FromGulsan Plaza Restaurant GPR<br/>
+                        Arriving in 20 - 30 min<br/>
+                        107 Rd No 8</p>
+       <div className='row align-items-center item-design py-2 my-3'>
+         
+          <div className="col-3">
+            <img src={img} className="img-cart" alt="" />
+          </div>
+          <div className="col-3">
+            <p>{name}</p>
+            <p>{price}</p>
+            </div>
+            <div className="col-1">
+              <h5><i class="fa fa-trash"></i></h5>
+            </div>
+            <div className="col-5">
+             <div className='d-flex align-items-center justify-content-between rounded-pill border-added px-3'>
+              <button className='border-area'><i class="fa fa-minus"></i></button>
+                <h5 className=''>0</h5>
+                <button className='border-area'><i class="fa fa-plus"></i></button>
+             </div>
+            </div>
+        </div>
+     </div>
   )
 }
 
